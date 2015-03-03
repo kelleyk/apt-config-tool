1a06bbcd26b64da6af56bdc32b35822459d75000
Kevin Kelley <kelleyk@kelleyk.net>
2015-03-03 13:57:08 -0800

    Can you tell I didn't sleep last night?

--- apt_config_tool/apt_config_tool.py
+++ apt_config_tool/apt_config_tool.py
@@ -89,7 +89,7 @@ def cmd_preprocess(args):
     proxy = get_apt_proxy()
 
     with open(args.input_file, 'r') as f:
-        data = f.read(yaml.load(f.read()))
+        data = yaml.load(f.read())
 
     output = [
         '#!/usr/bin/env bash',
